cc0d24ab6f56e1c1155bf9d0d3f533e25491b9f3
rbb <940774475@qq.com>
2019-08-09 16:01:53 +0800

    20190809更新

--- src/main.js
+++ src/main.js
@@ -1,17 +1,34 @@
 //这是入口文件
 import Vue from 'vue';
 
+//1.1导入路由包
+import VueRouter from 'vue-router';
+//1.2安装路由
+Vue.use(VueRouter);
+
+//2.1导入vue-resource
+import VueResource from 'vue-resource';
+//2.2安装vue-resource
+Vue.use(VueResource);
+
 //导入MUI样式
 import './lib/mui/mui-master/dist/css/mui.min.css'
+import './lib/mui/mui-master/examples/hello-mui/css/icons-extra.css'
 
 //按需导入mint-ui组件
-import {Header} from 'mint-ui';
+import {Header,Swipe, SwipeItem} from 'mint-ui';
 Vue.component(Header.name,Header);
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
+
+//1.3导入自己的router.js路由模块
+import router from './router';
 
 //导入根组件
 import app from './App.vue'
 
 var vm = new Vue({
     el:'#app',
-    render:c=>c(app)
+    render:c=>c(app),
+    router//挂载路由对象到VM实例上
 })
