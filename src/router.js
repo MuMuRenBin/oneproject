9da1556a7c8bc218264457f8713bea2c2881e43f
rbb <940774475@qq.com>
2019-08-09 19:52:19 +0800

    20190809

--- src/router.js
+++ src/router.js
@@ -5,6 +5,7 @@ import HomeContainer from './components/tabbar/HomeContainer.vue'
 import MemberContainer from './components/tabbar/MemberContainer.vue'
 import ShopcarContainer from './components/tabbar/ShopcarContainer.vue'
 import SearchContainer from './components/tabbar/SearchContainer.vue'
+import NewsList from './components/news/NewsList.vue'
 
 const router = new VueRouter({
     routes:[//配置路由规则的
@@ -13,6 +14,7 @@ const router = new VueRouter({
         {path:'/member',component:MemberContainer},
         {path:'/shopcar',component:ShopcarContainer},
         {path:'/search',component:SearchContainer},
+        {path:'/home/newslist',component:NewsList}
     ],
     linkActiveClass:'mui-active'//覆盖默认的路由高亮的类，默认的类叫router-link-active
 })
